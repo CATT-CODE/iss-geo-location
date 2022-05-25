6907b9a8c244b4a76926d82ae69a8e152ea6f3fb
dd-code-immersives <domilamad89@gmail.com>
2022-05-25 18:39:39 +0300

    added comments

--- app/views.py
+++ app/views.py
@@ -7,71 +7,85 @@ load_dotenv()
 URL1Secret = os.getenv('URL1SECURE')
 URL3Secret = os.getenv('URL3SECURE')
 
+URL = 'http://api.open-notify.org/iss-now.json'
+URL1A = 'http://api.open-notify.org/astros.json'
+URL2 = 'https://ipapi.co/json/'
+URL3 = "https://distance-calculator.p.rapidapi.com/distance/simple"
+
 def show_location(request):
-	if request.method == 'GET':
-		return render(request, 'app/base.html')
+	
 
 	if request.method == 'POST':
-			URL = 'http://api.open-notify.org/iss-now.json'
-
-			r = requests.get(url = URL)
-			data = r.json()
-
-			issLatitude = data['iss_position']['latitude']
-			issLongitude = data['iss_position']['longitude']
-
-			URL1 = URL1Secret
-			PARAMS = {'lat':issLatitude, 'lon':issLongitude}
 			
-			try:
-				r1 = requests.get(url = URL1, params = PARAMS)
-				data1 = r1.json()
-			except:
-				context = {'error': "API 1 ERROR"}
-				return render(request, 'app/base.html', context)
+
+		r = requests.get(url = URL)
+		data = r.json()
+
+		issLatitude = data['iss_position']['latitude']
+		issLongitude = data['iss_position']['longitude']
+
+		URL1 = URL1Secret
+		PARAMS = {'lat':issLatitude, 'lon':issLongitude}
+		
+		# think about putting this request calls in a helper function,
+		# see below
+		try:
+			r1 = requests.get(url = URL1, params = PARAMS)
+			data1 = r1.json()
+		except:
+			context = {'error': "API 1 ERROR"}
+			return render(request, 'app/base.html', context)
+		
+		
+		try:
+			r2 = requests.get(url = URL2)
+		except:
+			context = {'error': "API 2 ERROR"}
+			return render(request, 'app/base.html', context)
+
+		data2 = r2.json()
+		currCity = data2['city']
+		currLat = data2['latitude']
+		currLong = data2['longitude']
+
+		querystring = {
+			"lat_1": issLatitude, 
+			"long_1": issLongitude, 
+			"lat_2": currLat, 
+			"long_2": currLong, 
+			"unit": "miles", 
+			"decimal_places": "0"
+		}
+
+		headers = {
+			"Content-Type": "application/json",
+			"X-RapidAPI-Host": "distance-calculator.p.rapidapi.com",
+			"X-RapidAPI-Key": URL3Secret
+		}
+
+		try:
+			r3 = requests.request("GET", URL3, headers=headers, params=querystring)
+		except:
+			context = {'error': "API 3 ERROR"}
+			return render(request, 'app/base.html', context)
 			
-			URL2 = 'https://ipapi.co/json/'
+		data3 = r3.json()
+		distance = data3['distance']
 
-			try:
-				r2 = requests.get(url = URL2)
-			except:
-				context = {'error': "API 2 ERROR"}
-				return render(request, 'app/base.html', context)
+		if 'error' in data1:
+			context = {'water': "Over Water", 'distance': distance, 'currCity': currCity}
+			return render(request, 'app/base.html', context)
+		else:
+			context = {'country': data1['address']['country'], 'state': data1['address']['state'], 'distance': distance, 'currCity': currCity}
+			
+			return render(request, 'app/base.html', context)
 
-			data2 = r2.json()
-			currCity = data2['city']
-			currLat = data2['latitude']
-			currLong = data2['longitude']
-
-			URL3 = "https://distance-calculator.p.rapidapi.com/distance/simple"
-			querystring = {"lat_1": issLatitude, "long_1": issLongitude, "lat_2": currLat, "long_2": currLong, "unit": "miles", "decimal_places": "0"}
-
-			headers = {
-				"Content-Type": "application/json",
-				"X-RapidAPI-Host": "distance-calculator.p.rapidapi.com",
-				"X-RapidAPI-Key": URL3Secret
-			}
-
-			try:
-				r3 = requests.request("GET", URL3, headers=headers, params=querystring)
-			except:
-				context = {'error': "API 3 ERROR"}
-				return render(request, 'app/base.html', context)
-				
-			data3 = r3.json()
-			distance = data3['distance']
-
-			if 'error' in data1:
-				context = {'water': "Over Water", 'distance': distance, 'currCity': currCity}
-				return render(request, 'app/base.html', context)
-			else:
-				context = {'country': data1['address']['country'], 'state': data1['address']['state'], 'distance': distance, 'currCity': currCity}
-				
-				return render(request, 'app/base.html', context)
+	# GET is always default request
+	return render(request, 'app/base.html')
 
 def show_people(request):
-	URL1A = 'http://api.open-notify.org/astros.json'
-	
+		
+	# __make_request(...) * See below
 	try:
 		r1A = requests.get(url = URL1A)
 	except:
@@ -87,6 +101,19 @@ def show_people(request):
 
 	return render(request, 'app/peeps.html', context)
 
+
+# consider implementing a helper function request for the above requests
+# def __make_request(...):
+
+# 	try:
+# 		res = requests.get(url = URL1A)
+# 	except:
+# 		context = {'error': "API 1A ERROR"}
+
+# 	return res, context
+
+
+	
 
 
 
